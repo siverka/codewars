93ec7ada3c16c5942f12efa301a5022cdb3b6d78
siverka <siverka@gmail.com>
2018-02-20 00:11:04 +0300

    resolved Isogram kata

---
+++ Isograms/isograms.py
@@ -0,0 +1,30 @@
+"""
+https://www.codewars.com/kata/54ba84be607a92aa900000f1
+An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+Implement a function that determines whether a string that contains only letters is an isogram.
+Assume the empty string is an isogram. Ignore letter case.
+"""
+
+from unittest import TestCase
+
+
+def is_isogram(word: str):
+    word = word.lower()
+    if not word:
+        return True
+    else:
+        return len(set(word)) == len(word)
+
+
+class Test_Is_isogram(TestCase):
+    def test_is_isogram_1(self):
+        assert is_isogram('') == True
+
+    def test_is_isogram_2(self):
+        assert is_isogram("Dermatoglyphics") == True
+
+    def test_is_isogram_3(self):
+        assert is_isogram('aba') == False
+
+    def test_is_isogram_4(self):
+        assert is_isogram('moOse') == False
