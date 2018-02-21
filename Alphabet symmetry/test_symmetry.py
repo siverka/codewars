52898fb1872209a5d7fce3874f3d88149ebf32bd
Maria Korobeynikova <siverka@gmail.com>
2018-02-21 18:09:30 +0300

    resolved 'Alphabet symmetry' kata

---
+++ Alphabet symmetry/test_symmetry.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from alphabet_symmetry import symmetry
+
+class TestSymmetry(TestCase):
+    def test_symmetry_1(self):
+        assert symmetry(["abode","ABc","xyzD"]) == [4, 3, 1]
+
+    def test_symmetry_2(self):
+        assert symmetry(["abide","ABc","xyz"]) == [4, 3, 0]
+
+    def test_symmetry_3(self):
+        assert symmetry(["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"]) == [6, 5, 7]
+
+    def test_symmetry_3(self):
+        assert symmetry(["encode","abc","xyzD","ABmD"]) == [1, 3, 1, 3]
