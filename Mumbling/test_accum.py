f610f577a5a9f4c046bfa8796d8134239fa385e8
Maria Korobeynikova <siverka@gmail.com>
2018-02-22 11:36:42 +0300

    resolved 'Mumbling' kata

---
+++ Mumbling/test_accum.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from mumbling import accum
+
+class TestAccum(TestCase):
+    def test_accum_1(self):
+        assert accum("ZpglnRxqenU") == "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+
+    def test_accum_2(self):
+        assert accum("NyffsGeyylB") == "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+
+    def test_accum_3(self):
+        assert accum("MjtkuBovqrU") == "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+
+    def test_accum_4(self):
+        assert accum("EvidjUnokmM") == "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+
+    def test_accum_5(self):
+        assert accum("HbideVbxncC") == "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+
