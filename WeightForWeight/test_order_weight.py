9fa5952688d10d8313c896d7d8ea95427e5ee7b7
Maria Korobeynikova <siverka@gmail.com>
2018-02-26 23:27:22 +0300

    resolved 'Weight for weight' kata

---
+++ WeightForWeight/test_order_weight.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from WeightForWeight.weight_for_weight import order_weight
+
+
+class TestOrderWeight(TestCase):
+    def test_order_weight(self):
+        self.fail()
